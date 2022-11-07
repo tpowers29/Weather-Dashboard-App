0518f3552bb5a0b87dabfc621aea1faed0a0dee6
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2022-11-06 20:04:41 -0500

    added api's

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+var searchbutton= document.getElementById("search")
+const apikey="f94df6e9e239910196e39d5e3081678c"
+
+function get5dayforcast (cityName){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apikey}&units=imperial`)
+    .then(function(data){
+        return data.json()
+    }).then(function(apiresults){
+        console.log(apiresults)
+        var htmlstring = `<div class="card" style="width: 18rem;">
+        <h2>${city}</h2>
+        <img src="http://openweathermap.org/img/wn/${apiresults.list[0].weather[0].icon}@2x.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Temperature: ${apiresults.list[0].main.temp}</p>
+          <p class="card-text">Temperature: ${apiresults.list[0].wind.speed}</p>
+          <p class="card-text">Temperature: ${apiresults.list[0].weather[0].description}</p>
+          <p class="card-text">Temperature: ${apiresults.list[0].main.humidity}</p>
+        </div>
+      </div>`
+      document.getElementById("current").innerHTML = htmlstring
+
+    })
+
+}
+
+
+searchbutton.addEventListener ("click",function(event){
+    event.preventDefault()
+    var cityName = document.getElementById("city-name").value
+    console.log(cityName)
+    get5dayforcast(cityName)
+}
+)
+
