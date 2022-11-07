81f113e530abc207ed25e09c24eaa7660b6bfef7
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2022-11-06 20:13:04 -0500

    5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,9 @@ function get5dayforcast (cityName){
         return data.json()
     }).then(function(apiresults){
         console.log(apiresults)
-        var htmlstring = `<div class="card" style="width: 18rem;">
-        <h2>${city}</h2>
+        var htmlstring = `
+        <div class="card" style="width: 18rem;">
+        <h2>${cityName}</h2>
         <img src="http://openweathermap.org/img/wn/${apiresults.list[0].weather[0].icon}@2x.png" class="card-img-top" alt="...">
         <div class="card-body">
           <p class="card-text">Temperature: ${apiresults.list[0].main.temp}</p>
@@ -18,7 +19,23 @@ function get5dayforcast (cityName){
         </div>
       </div>`
       document.getElementById("current").innerHTML = htmlstring
+      var html = ""
+        for(let i=8; i<apiresults.list.length;i=i+8){
+            html += `<div class="card" style="width: 18rem;">
+          
+            <img src="http://openweathermap.org/img/wn/${apiresults.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Temperature: ${apiresults.list[i].main.temp}</p>
+              <p class="card-text">Temperature: ${apiresults.list[i].wind.speed}</p>
+              <p class="card-text">Temperature: ${apiresults.list[i].weather[0].description}</p>
+              <p class="card-text">Temperature: ${apiresults.list[i].main.humidity}</p>
+            </div>
+          </div>
+            
+            `
+            document.getElementById ("five").innerHTML = html
 
+        }
     })
 
 }
